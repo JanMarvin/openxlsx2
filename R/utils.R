d8b6c068e33af263103a7e50744cd8ec9552c0ea
jmbarbone <jmbarbone@gmail.com>
2021-12-26 15:51:33 -0500

    adds null utils

--- R/utils.R
+++ R/utils.R
@@ -9,3 +9,10 @@ is_integer_ish <- function(x) {
 
   all(x[!is.na(x)] %% 1 == 0)
 }
+
+`%||%` <- function(x, y) if (is.null(x)) y else x
+
+na_to_null <- function(x) {
+  lapply(x, function(i) if (is.na(i)) NULL else i)
+}
+
