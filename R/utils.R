50d65829f38b4cc5ab1e4ea0f9e824ee0c463b20
jmbarbone <jmbarbone@gmail.com>
2021-12-26 15:51:40 -0500

    adds paste_c util

--- R/utils.R
+++ R/utils.R
@@ -16,3 +16,10 @@ na_to_null <- function(x) {
   lapply(x, function(i) if (is.na(i)) NULL else i)
 }
 
+# useful for replacing multiple x <- paste(x, new) where the length is checked
+# multiple times.  This combines all elements in ... and removes anything that
+# is zero length
+paste_c <- function(..., sep = "", collapse = " ") {
+  x <- c(...)
+  paste(x[nzchar(x)], sep = "", collapse = " ")
+}
