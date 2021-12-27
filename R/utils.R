40a5551bb6469cc6c1f05c26b9f235bfd812e477
jmbarbone <jmbarbone@gmail.com>
2021-12-27 14:42:11 -0500

    adds paste_c() util
    to be incorporated and tested later

--- R/utils.R
+++ R/utils.R
@@ -13,3 +13,11 @@ is_integer_ish <- function(x) {
 naToNULLList <- function(x) {
   lapply(x, function(i) if (is.na(i)) NULL else i)
 }
+
+# useful for replacing multiple x <- paste(x, new) where the length is checked
+# multiple times.  This combines all elements in ... and removes anything that
+# is zero length
+paste_c <- function(..., sep = "", collapse = " ") {
+  x <- c(...)
+  paste(x[nzchar(x)], sep = "", collapse = " ")
+}
---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,8 @@
+
+test_that("paste_c() works", {
+  res <- paste_c("a", "", "b", NULL, "c", " ")
+  exp <- "a b c  "
+  expect_identical(res, exp)
+
+  expect_identical(paste_c(character(), "", NULL), "")
+})
