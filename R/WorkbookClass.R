a89ac7456d88c854ad64fe9ec9b110c567a9d4f3
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2021-12-28 23:25:40 +0100

    restore writing files

--- R/WorkbookClass.R
+++ R/WorkbookClass.R
@@ -3386,53 +3386,8 @@ Workbook$methods(
 
 
         ## convert sheet name to index
-        sheet <- which(sheet_names == x$sheet)
-        sId <-
-          .self$updateStyles(this.sty) ## this creates the XML for styles.XML
+        sId <- .self$updateStyles(this.sty) ## this creates the XML for styles.XML
 
-        cells_to_style <- stri_join(x$rows, x$cols, sep = ",")
-        existing_cells <-
-          stri_join(worksheets[[sheet]]$sheet_data$rows,
-            worksheets[[sheet]]$sheet_data$cols,
-            sep = ","
-          )
-
-        ## In here we create any style_ids that don't yet exist in sheet_data
-        #worksheets[[sheet]]$sheet_data$style_id[existing_cells %in% cells_to_style] <<-
-        #  sId
-
-
-        new_cells_to_append <-
-          which(!cells_to_style %in% existing_cells)
-        if (length(new_cells_to_append) > 0) {
-          #worksheets[[sheet]]$sheet_data$style_id <<-
-          #  c(
-          #    worksheets[[sheet]]$sheet_data$style_id,
-          #    rep.int(x = sId, times = length(new_cells_to_append))
-          #  )
-
-          worksheets[[sheet]]$sheet_data$rows <<-
-            c(worksheets[[sheet]]$sheet_data$rows, x$rows[new_cells_to_append])
-          worksheets[[sheet]]$sheet_data$cols <<-
-            c(worksheets[[sheet]]$sheet_data$cols, x$cols[new_cells_to_append])
-          worksheets[[sheet]]$sheet_data$t <<-
-            c(worksheets[[sheet]]$sheet_data$t, rep(as.integer(NA), length(new_cells_to_append)))
-          worksheets[[sheet]]$sheet_data$v <<-
-            c(
-              worksheets[[sheet]]$sheet_data$v,
-              rep(as.character(NA), length(new_cells_to_append))
-            )
-          worksheets[[sheet]]$sheet_data$f <<-
-            c(
-              worksheets[[sheet]]$sheet_data$f,
-              rep(as.character(NA), length(new_cells_to_append))
-            )
-          worksheets[[sheet]]$sheet_data$data_count <<-
-            worksheets[[sheet]]$sheet_data$data_count + 1L
-
-          worksheets[[sheet]]$sheet_data$n_elements <<-
-            as.integer(length(worksheets[[sheet]]$sheet_data$rows))
-        }
       }
     }
 
