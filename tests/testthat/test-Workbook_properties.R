eb1934d7adf5125a3354c1b474291c2f28b1003b
jmbarbone <jmbarbone@gmail.com>
2022-02-06 13:15:37 -0500

    update workbook property tests

--- tests/testthat/test-Workbook_properties.R
+++ tests/testthat/test-Workbook_properties.R
@@ -1,39 +1,53 @@
 
 
 test_that("Workbook properties", {
+  # TODO check datetimeCreated field
 
   ## check creator
-  wb <- wb_workbook(creator = "Alex", title = "title here", subject = "this & that", category = "some category")
+  wb <- wb_workbook(
+    creator  = "Alex",
+    title    = "title here",
+    subject  = "this & that",
+    category = "some category"
+  )
 
-  expect_true(grepl("<dc:creator>Alex</dc:creator>", wb$core))
-  expect_true(grepl("<dc:title>title here</dc:title>", wb$core))
-  expect_true(grepl("<dc:subject>this &amp; that</dc:subject>", wb$core))
-  expect_true(grepl("<cp:category>some category</cp:category>", wb$core))
+  expect_match(wb$core, "<dc:creator>Alex</dc:creator>")
+  expect_match(wb$core, "<dc:title>title here</dc:title>")
+  expect_match(wb$core, "<dc:subject>this &amp; that</dc:subject>")
+  expect_match(wb$core, "<cp:category>some category</cp:category>")
 
-  fl <- tempfile(fileext = ".xlsx")
+  temp_file <- temp_xlsx()
   wb <- write.xlsx(
-    x = iris, file = fl,
-    creator = "Alex 2",
-    title = "title here 2",
-    subject = "this & that 2",
+    x        = iris,
+    file     = temp_file,
+    creator  = "Alex 2",
+    title    = "title here 2",
+    subject  = "this & that 2",
     category = "some category 2"
   )
 
-  expect_true(grepl("<dc:creator>Alex 2</dc:creator>", wb$core))
-  expect_true(grepl("<dc:title>title here 2</dc:title>", wb$core))
-  expect_true(grepl("<dc:subject>this &amp; that 2</dc:subject>", wb$core))
-  expect_true(grepl("<cp:category>some category 2</cp:category>", wb$core))
+  expect_match(wb$core, "<dc:creator>Alex 2</dc:creator>")
+  expect_match(wb$core, "<dc:title>title here 2</dc:title>")
+  expect_match(wb$core, "<dc:subject>this &amp; that 2</dc:subject>")
+  expect_match(wb$core, "<cp:category>some category 2</cp:category>")
 
   ## maintain on load
-  wb_loaded <- loadWorkbook(fl)
-  expect_equal(object = wb_loaded$core, expected = paste0(wb$core, collapse = ""))
+  wb_loaded <- loadWorkbook(temp_file)
+  expect_equal(wb_loaded$core, paste0(wb$core, collapse = ""))
 
+  wb <- wb_workbook(
+    creator  = "Philipp",
+    title    = "title here",
+    subject  = "this & that",
+    category = "some category"
+  )
 
-  wb <- wb_workbook(creator = "Philipp", title = "title here", subject = "this & that", category = "some category")
   addCreator(wb, "test")
-  expect_true(grepl("<dc:creator>Philipp;test</dc:creator>", wb$core))
+  expect_match(wb$core, "<dc:creator>Philipp;test</dc:creator>")
 
-  expect_equal(getCreators(wb), c("Philipp", "test"))
+  expect_identical(getCreators(wb), c("Philipp", "test"))
   setLastModifiedBy(wb, "Philipp 2")
-  expect_true(grepl("<cp:lastModifiedBy>Philipp 2</cp:lastModifiedBy>", wb$core))
+  expect_match(wb$core, "<cp:lastModifiedBy>Philipp 2</cp:lastModifiedBy>")
+
+  file.remove(temp_file)
 })
