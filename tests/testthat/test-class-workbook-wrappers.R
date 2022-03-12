c5c517b0e081d90a513a9a1f6e28d41cba25f11c
jmbarbone <jmbarbone@gmail.com>
2022-03-12 17:41:13 -0500

    implement some tests
    #84

--- tests/testthat/test-class-workbook-wrappers.R
+++ tests/testthat/test-class-workbook-wrappers.R
@@ -1,3 +1,22 @@
-test_that("Workbook class wrappers work", {
-  skip("no tests yet")
+
+
+test_that("wrappers are wrappers", {
+
+  # wb_workbook() ----
+  ts <- Sys.time()
+  expect_equal(wb_workbook(datetimeCreated = ts), wbWorkbook$new(datetimeCreated = ts))
+  expect_wrapper("initialize", "wb_workbook", params = NULL)
+
+  # wb_add_worksheet() ----
+  expect_wrapper("addWorksheet", "wb_add_worksheet", params = list(sheetName = "this"))
+
+  # wb_save() ----
+  # returns the file path instead
+  expect_wrapper("save", "wb_save", params = NULL, ignore = "path")
+
+  # wb_merge_cells(), wb_unmerge_cells() ----
+  wb <- wbWorkbook$new()$addWorksheet("sheet")
+  params <- list(sheet = "sheet", rows = 1:2, cols = 2)
+  expect_wrapper("addCellMerge",    "wb_merge_cells",   wb = wb, params = params)
+  expect_wrapper("removeCellMerge", "wb_unmerge_cells", wb = wb, params = params)
 })
