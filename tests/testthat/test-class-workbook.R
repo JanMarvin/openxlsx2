651596aaed756b1b3e2fc187f04eacd7cdc08c5d
jmbarbone <jmbarbone@gmail.com>
2022-04-19 23:41:03 -0400

    update function names

--- tests/testthat/test-class-workbook.R
+++ tests/testthat/test-class-workbook.R
@@ -7,28 +7,28 @@ test_that("wb_set_col_widths", {
 # TODO use wb$wb_set_col_widths()
 
   wb <- wbWorkbook$new()
-  wb$add_worksheet("test")
+  wb <- wb$add_worksheet("test")
   writeData(wb, "test", mtcars)
 
   # set column width to 12
-  expect_silent(wb_set_col_widths(wb, "test", widths = 12L, cols = seq_along(mtcars)))
+  expect_silent(wb$set_col_widths("test", widths = 12L, cols = seq_along(mtcars)))
   expect_equal(
     "<col min=\"1\" max=\"11\" width=\"12\" bestFit=\"1\" customWidth=\"1\" hidden=\"false\"/>",
     wb$worksheets[[1]]$cols_attr
   )
 
   # wrong sheet
-  expect_error(wb_set_col_widths(wb, "test2", widths = 12L, cols = seq_along(mtcars)))
+  expect_error(wb$set_col_widths("test2", widths = 12L, cols = seq_along(mtcars)))
 
   # reset the column with, we do not provide an option ot remove the column entry
-  expect_silent(wb_set_col_widths(wb, "test", cols = seq_along(mtcars)))
+  expect_silent(wb$set_col_widths("test", cols = seq_along(mtcars)))
   expect_equal(
     "<col min=\"1\" max=\"11\" width=\"8.43\" bestFit=\"1\" customWidth=\"1\" hidden=\"false\"/>",
     wb$worksheets[[1]]$cols_attr
   )
 
   # create column width for column 25
-  expect_silent(wb_set_col_widths(wb, "test", cols = "Y", widths = 22))
+  expect_silent(wb$set_col_widths("test", cols = "Y", widths = 22))
   expect_equal(
     c("<col min=\"1\" max=\"11\" width=\"8.43\" bestFit=\"1\" customWidth=\"1\" hidden=\"false\"/>",
       "<col min=\"12\" max=\"24\" width=\"8.43\"/>",
@@ -37,6 +37,6 @@ test_that("wb_set_col_widths", {
   )
 
   # a few more errors
-  expect_error(wb_set_col_widths(wb, "test", cols = "Y", widths = 1:2))
-  expect_error(wb_set_col_widths(wb, "test", cols = "Y", hidden = 1:2))
+  expect_error(wb$set_col_widths("test", cols = "Y", width = 1:2))
+  expect_error(wb$set_col_widths("test", cols = "Y", hidden = 1:2))
 })
