dc59705b30b2de22f483b0f36fcf5c0a98cf55fd
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2022-04-22 18:50:34 +0200

    fix test

--- tests/testthat/test-cloneWorksheet.R
+++ tests/testthat/test-cloneWorksheet.R
@@ -1,17 +1,16 @@
-
-
 test_that("clone Worksheet with data", {
   wb <- wb_workbook()
   wb$add_worksheet("Sheet 1")
   writeData(wb, "Sheet 1", 1)
   wb$clone_worksheet("Sheet 1", "Sheet 2")
 
-  file_name <- system.file("extdata", "clone_worksheetExample.xlsx", package = "openxlsx2")
+  file_name <- system.file("extdata", "cloneWorksheetExample.xlsx", package = "openxlsx2")
   refwb <- wb_load(file = file_name)
 
   expect_equal(names(wb), names(refwb))
   expect_equal(worksheetOrder(wb), worksheetOrder(refwb))
 })
+
 
 test_that("clone empty Worksheet", {
   wb <- wb_workbook()
