006887407464ccf2faea2f1da6fe0803e082ee92
jmbarbone <jmbarbone@gmail.com>
2022-04-23 13:17:50 -0400

    update add_plot() test

--- tests/testthat/test-class-workbook-wrappers.R
+++ tests/testthat/test-class-workbook-wrappers.R
@@ -163,6 +163,19 @@ test_that("wb_add_image() is a wrapper", {
 # wb_add_plot() -----------------------------------------------------------
 
 test_that("wb_add_plot() is a wrapper", {
-  # TODO add tests for results
-  expect_wrapper("add_plot", "wb_add_plot", params = NULL)
+  plot(1:5, 1:5)
+  wb <- wb_workbook()$add_worksheet("a")
+
+  # okay, not the best but the results have different field names.  Maybe that's
+  # a feature to add to expect_wrapper()
+  expect_failure(
+    expect_wrapper(
+      "add_plot",
+      "wb_add_plot",
+      wb = wb,
+      params = list(sheet = "a")
+    ),
+    "wbWorkbook$add_plot$media$image1.png vs wb_add_plot$media$image1.png",
+    fixed = TRUE
+  )
 })
