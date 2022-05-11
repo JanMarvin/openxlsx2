4285bef7d72eb4ef4c6a3d65b3ca5d3557c90af3
jmbarbone <jmbarbone@gmail.com>
2022-05-10 21:14:49 -0400

    clean up xl_open()
    * adds shell.exec() for windows method
    * formats if .. else statements (some had stops() so the rest of the evaluation was moot)
    * uses switch() for the userSystem checks
    * uses S3 dispatching for wbWorkbook
    * uses clone() of workbook as to not overwrite the current path
    * adds interactive param to control interactive() checking

--- R/xl_open.R
+++ R/xl_open.R
@@ -1,23 +1,25 @@
-#' @name xl_open
-#' @title Open a Microsoft Excel file (xls/xlsx) or an openxlsx Workbook
-#' @description This function tries to open a Microsoft Excel
-#' (xls/xlsx) file or an openxlsx Workbook with the proper
-#' application, in a portable manner.
+#' Open a Microsoft Excel file (xls/xlsx) or an openxlsx Workbook
 #'
-#' In Windows (c) and Mac (c), it uses system default handlers,
-#' given the file type.
+#' @description This function tries to open a Microsoft Excel (xls/xlsx) file or
+#'   an openxlsx Workbook with the proper application, in a portable manner.
 #'
-#' In Linux it searches (via `which`) for available xls/xlsx
-#' reader applications (unless `options('openxlsx.excelApp')`
-#' is set to the app bin path), and if it finds anything, sets
-#' `options('openxlsx.excelApp')` to the program chosen by
-#' the user via a menu (if many are present, otherwise it will
-#' set the only available). Currently searched for apps are
-#' Libreoffice/Openoffice (`soffice` bin), Gnumeric
-#' (`gnumeric`) and Calligra Sheets (`calligrasheets`).
+#'   In Windows it uses [base::shell.exec()] to determine the appropriate
+#'   program.
+#'
+#'   In Mac (c) it uses system default handlers, given the file type.
+#'
+#'   In Linux it searches (via `which`) for available xls/xlsx reader
+#'   applications (unless `options('openxlsx.excelApp')` is set to the app bin
+#'   path), and if it finds anything, sets `options('openxlsx.excelApp')` to the
+#'   program chosen by the user via a menu (if many are present, otherwise it
+#'   will set the only available). Currently searched for apps are
+#'   Libreoffice/Openoffice (`soffice` bin), Gnumeric (`gnumeric`) and Calligra
+#'   Sheets (`calligrasheets`).
 #'
 #' @param file path to the Excel (xls/xlsx) file or Workbook object.
-#' @usage xl_open(file=NULL)
+#' @param interactive If `FALSE` will throw a warning and not open the path.
+#'   This can be manually set to `TRUE`, otherwise when `NA` (defualt) uses the
+#'   value returned from [base::interactive()]
 #' @export
 #' @examples
 #' # file example
@@ -30,42 +32,52 @@
 #' wb$add_worksheet("Cars")
 #' wb$add_data("Cars", x, startCol = 2, startRow = 3, rowNames = TRUE)
 #' xl_open(wb)
-xl_open <- function(file = NULL) {
+xl_open <- function(file = NULL, interactive = NA) {
+  UseMethod("xl_open")
+}
 
-  if (!interactive()) return()
+#' @rdname xl_open
+#' @export
+xl_open.wbWorkbook <- function(file = NULL, interactive = NA) {
+  stopifnot(R6::is.R6(file))
+  xl_open(file$clone()$save(temp_xlsx())$path)
+}
 
-  op <- openxlsx_options()
-  on.exit(options(op), add = TRUE)
-
-  if (is.null(file)) stop("A file has to be specified.")
-
-  ## workbook handling
-  if (inherits(file, "wbWorkbook")) {
-    file <- file$save(path = temp_xlsx())$path
+#' @rdname xl_open
+#' @export
+xl_open.default <- function(file = NULL, interactive = NA) {
+  if (is.na(interactive)) {
+    interactive <- interactive()
   }
 
-  if (!file.exists(file)) stop("Non existent file or wrong path.")
+  if (!isTRUE(interactive)) {
+    warning("will not open file when not interactive")
+    return()
+  }
+
+  stopifnot(!is.null(file), file.exists(file))
 
   ## execution should be in background in order to not block R
   ## interpreter
-  file <- normalizePath(file)
+  file <- normalizePath(file, mustWork = TRUE)
   userSystem <- Sys.info()["sysname"]
 
-
-  if ("Linux" == userSystem) {
-    if (is.null(app <- unlist(options("openxlsx2.excelApp")))) {
-      app <- chooseExcelApp()
-    }
-    myCommand <- paste(app, file, "&", sep = " ")
-    system(command = myCommand)
-  } else if ("Windows" == userSystem) {
-    shell(shQuote(file, "sh"), wait = FALSE) # nolint
-  } else if ("Darwin" == userSystem) {
-    myCommand <- paste0('open ', shQuote(file))
-    system(command = myCommand)
-  } else {
-    warning("Operating system not handled.")
-  }
+  switch(
+    userSystem,
+    Linux = {
+      app <- getOption("openxlsx2.excelApp", chooseExcelApp())
+      myCommand <- paste(app, file, "&", sep = " ")
+      system(command = myCommand)
+    },
+    Windows = {
+      shell.exec(file)
+    },
+    Darwin = {
+      myCommand <- paste0('open ', shQuote(file))
+      system(command = myCommand)
+    },
+    stop("Operating system not handled: ", toString(userSystem))
+  )
 }
 
 
@@ -78,19 +90,25 @@ chooseExcelApp <- function() {
 
   prog <- Sys.which(m)
   names(prog) <- names(m)
-  nApps <- length(availProg <- prog["" != prog])
+  availProg <- prog["" != prog]
+  nApps <- length(availProg)
 
   if (0 == nApps) {
     stop(
       "No applications (detected) available.\n",
       "Set options('openxlsx.excelApp'), instead."
     )
-  } else if (1 == nApps) {
-    cat("Only", names(availProg), "found; I'll use it.\n")
+  }
+
+  if (1 == nApps) {
+    message("Only ", names(availProg), " found")
     unnprog <- unname(availProg)
-    options(openxlsx.excelApp = unnprog)
+    message(sprintf("Setting options(openxlsx2.excelApp = '%s')", unnprog))
+    options(openxlsx2.excelApp = unnprog)
     invisible(unnprog)
-  } else if (1 < nApps) {
+  }
+
+  if (1 < nApps) {
     if (!interactive()) {
       stop(
         "Cannot choose an Excel file opener non-interactively.\n",
@@ -99,9 +117,12 @@ chooseExcelApp <- function() {
     }
     res <- menu(names(availProg), title = "Excel Apps availables")
     unnprog <- unname(availProg[res])
-    if (res > 0L) options(openxlsx.excelApp = unnprog)
+    if (res > 0L) {
+      message(sprintf("Setting options(openxlsx2.excelApp = '%s')", unnprog))
+      options(openxlsx2.excelApp = unnprog)
+    }
     invisible(unname(unnprog))
-  } else {
-    stop("Unexpected error.")
   }
+
+  stop("Unexpected error in openxlsx2::chooseExcelApp()")
 }
