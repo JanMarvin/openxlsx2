fd1c114c3a2285be55f096ad37cf4fded96bcce5
jmbarbone <jmbarbone@gmail.com>
2022-05-15 21:26:22 -0400

    remove commented out code

--- tests/testthat/test-Worksheet_naming.R
+++ tests/testthat/test-Worksheet_naming.R
@@ -1,36 +1,9 @@
 
-
 test_that("Worksheet names", {
-
   ### test for names without special character
   sheetname <- "test"
   wb <- wb_add_worksheet(wb_workbook(), sheetname)
   expect_identical(sheetname, names(wb))
-
-  # ### test for names with &
-  # sheetname <- "S&P 500"
-  # wb <- wb_add_worksheet(wb_workbook(), sheetname)
-  # expect_identical(sheetname, names(wb))
-  # expect_identical("S&amp;P 500", wb$sheet_names)
-  #
-  # ### test for names with <
-  # sheetname <- "<24 h"
-  # wb <- wb_add_worksheet(wb_workbook(), sheetname)
-  #
-  # expect_identical(sheetname, names(wb))
-  # expect_identical("&lt;24 h",wb$sheet_names)
-  #
-  # ### test for names with >
-  # sheetname <- ">24 h"
-  # wb <- wb_add_worksheet(wb_workbook(), sheetname)
-  # expect_identical(sheetname, names(wb))
-  # expect_identical("&gt;24 h", wb$sheet_names)
-  #
-  # ### test for names with "
-  # sheetname <- 'test "A"'
-  # wb <- wb_add_worksheet(wb_workbook(), sheetname)
-  # expect_identical(sheetname, names(wb))
-  # expect_identical("test &quot;A&quot;", wb$sheet_names)
 })
 
 test_that("worksheet names with illegal characters not allowed", {
