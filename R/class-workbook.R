3455ebc4e1b28376fa591c5aa9f4a9f7a92bf5c5
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2022-05-22 17:16:41 +0200

    [R/styles.xml] fix strangely broken styles.xml header and add xr9.

--- R/class-workbook.R
+++ R/class-workbook.R
@@ -1373,7 +1373,7 @@ wbWorkbook <- R6::R6Class(
       ## write styles.xml
       #if (class(self$styles_xml) == "uninitializedField") {
       write_file(
-        head = '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" mc:Ignorable="x14ac x16r2 xr" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision">',
+        head = '<styleSheet xmlns="http://schemas.openxmlformats.org/spreadsheetml/2006/main" xmlns:mc="http://schemas.openxmlformats.org/markup-compatibility/2006" xmlns:x14ac="http://schemas.microsoft.com/office/spreadsheetml/2009/9/ac" xmlns:x16r2="http://schemas.microsoft.com/office/spreadsheetml/2015/02/main" xmlns:xr="http://schemas.microsoft.com/office/spreadsheetml/2014/revision" xmlns:xr9="http://schemas.microsoft.com/office/spreadsheetml/2016/revision9" mc:Ignorable="x14ac x16r2 xr xr9">',
         body = pxml(styleXML),
         tail = "</styleSheet>",
         fl = file.path(xlDir, "styles.xml")
