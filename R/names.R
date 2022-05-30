524f4e9c6b30853ed60183778a8096fd46a5cbf4
jmbarbone <jmbarbone@gmail.com>
2022-05-30 11:41:12 -0400

    remove examples with names()

--- R/names.R
+++ R/names.R
@@ -4,17 +4,6 @@
 #'
 #' @param x A `wbWorkbook` object
 #' @export
-#' @examples
-#'
-#' wb <- wb_workbook()
-#' wb$add_worksheet("S1")
-#' wb$add_worksheet("S2")
-#' wb$add_worksheet("S3")
-#'
-#' names(wb)
-#' names(wb)[[2]] <- "S2a"
-#' names(wb)
-#' names(wb) <- paste("Sheet", 1:3)
 names.wbWorkbook <- function(x) {
   .Deprecated("$get_sheet_names()")
   assert_workbook(x)
