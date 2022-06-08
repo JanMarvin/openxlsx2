9a4188a5c4961d1660813cffb4264ba8cf8cb81e
jmbarbone <jmbarbone@gmail.com>
2022-06-08 09:24:34 -0400

    use updated private method

--- R/write.R
+++ R/write.R
@@ -625,7 +625,7 @@ write_data_table <- function(
     stop("sep must be a character vector of length 1")
 
   # TODO clean up when moved into wbWorkbook
-  sheet <- wb$.__enclos_env__$private$get_sheet(sheet)
+  sheet <- wb$.__enclos_env__$private$get_sheet_index(sheet)
   # sheet <- wb$validate_sheet(sheet)
 
   if (wb$isChartSheet[[sheet]]) stop("Cannot write to chart sheet.")
