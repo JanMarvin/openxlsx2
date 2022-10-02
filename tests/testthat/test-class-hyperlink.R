31fdee0ae24511b427c27c8de2b07f96c30807eb
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2022-10-02 12:11:19 +0200

    update test

--- tests/testthat/test-class-hyperlink.R
+++ tests/testthat/test-class-hyperlink.R
@@ -5,8 +5,8 @@ test_that("class Hyperlink works", {
 
 test_that("encode Hyperlink works", {
 
-  formula_old <- '= HYPERLINK("#Tab_1!" &amp; CELL("address", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), "Go to the selected column")'
-  formula_new <- '= HYPERLINK("#Tab_1!" & CELL("address", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), "Go to the selected column")'
+  formula_old <- '=HYPERLINK("#Tab_1!" &amp; CELL("address", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), "Go to the selected column")'
+  formula_new <- '=HYPERLINK("#Tab_1!" & CELL("address", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), "Go to the selected column")'
 
   wb <- wb_workbook()$
     add_worksheet("Tab_1", zoom = 80, gridLines = FALSE)$
@@ -16,7 +16,7 @@ test_that("encode Hyperlink works", {
     add_formula(dims = "B1", x = formula_old)$
     add_formula(dims = "B2", x = formula_new)
 
-  exp <- "= HYPERLINK(\"#Tab_1!\" &amp; CELL(\"address\", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), \"Go to the selected column\")"
+  exp <- "=HYPERLINK(\"#Tab_1!\" &amp; CELL(\"address\", INDEX(C1:F1, MATCH(A1, C1:F1, 0))), \"Go to the selected column\")"
   got <- wb$worksheets[[1]]$sheet_data$cc["11", "f"]
   expect_equal(exp, got)
 
