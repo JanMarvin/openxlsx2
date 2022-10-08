d0b3438f6a2c5610bc805cf6512ab36f85ee49f9
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2022-10-08 09:46:07 +0200

    improve dims_to_dataframe allow dims to be "A1:A2;C1:C2"

--- tests/testthat/test-wb_functions.R
+++ tests/testthat/test-wb_functions.R
@@ -163,3 +163,19 @@ test_that("select_active_sheet", {
   expect_identical(1, wb_get_active_sheet(wb))
 
 })
+
+
+test_that("dims_to_dataframe", {
+
+  exp <- structure(
+    list(A = c("A1", "A2"), C = c("C1", "C2")),
+    row.names = 1:2,
+    class = "data.frame"
+  )
+  got <- dims_to_dataframe("A1:A2;C1:C2", fill = TRUE)
+  expect_equal(exp, got)
+
+  got <- dims_to_dataframe("A1;A2;C1;C2", fill = TRUE)
+  expect_equal(exp, got)
+
+})
