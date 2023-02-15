f84797cdcbe67080dc1789e7f0aa95981541163a
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2023-02-15 12:13:40 +0100

    try to avoid passing long xml strings to file.exists()

--- R/pugixml.R
+++ R/pugixml.R
@@ -47,7 +47,7 @@ read_xml <- function(xml, pointer = TRUE, escapes = FALSE, declaration = FALSE, 
   isvml <- grepl("^.vml$", xml)
 
   isfile <- FALSE
-  if (length(xml) == 1 && file.exists(xml))
+  if (length(xml) == 1 && !is_xml(xml) && file.exists(xml))
     isfile <- TRUE
 
   if (!isfile)
