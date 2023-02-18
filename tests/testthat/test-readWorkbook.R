fa334bd9376a0c28a320e43ac9ed5c2b13d368a4
Jordan Mark Barbone <jmbarbone@gmail.com>
2023-02-18 11:28:45 -0500

    improve tests

--- tests/testthat/test-readWorkbook.R
+++ tests/testthat/test-readWorkbook.R
@@ -1,31 +1,50 @@
 
 test_that("check_xlsx_path() gives correct errors [#477]", {
-  x <- tempfile()
-  expect_error(check_xlsx_path(x))
-
+  # good
   x <- tempfile(fileext = ".XLSX")
   file.create(x)
   expect_error(check_xlsx_path(x), NA)
   file.remove(x)
 
-  x <- tempfile(fileext = ".zip")
+  x <- tempfile(fileext = ".xltx")
   file.create(x)
-  expect_warning(check_xlsx_path(x))
-  expect_warning(check_xlsx_path(x, warn = FALSE), NA)
+  expect_error(check_xlsx_path(x), NA)
   file.remove(x)
+
+  x <- tempfile(fileext = ".xlsm")
+  file.create(x)
+  expect_error(check_xlsx_path(x), NA)
+  file.remove(x)
+
+  x <- tempfile(fileext = ".xltm")
+  file.create(x)
+  expect_error(check_xlsx_path(x), NA)
+  file.remove(x)
+
+  # bad
+  x <- tempfile()
+  expect_error(check_xlsx_path(x))
 
   x <- tempfile(fileext = ".xls")
   file.create(x)
   expect_error(check_xlsx_path(x))
   file.remove(x)
 
-  x <- tempfile(fileext = ".xlsm")
+  x <- tempfile(fileext = ".xlm")
   file.create(x)
   expect_error(check_xlsx_path(x))
   file.remove(x)
 
+  # unknown
   x <- tempfile(fileext = ".xlsb")
   file.create(x)
-  expect_error(check_xlsx_path(x))
+  expect_warning(check_xlsx_path(x))
+  file.remove(x)
+
+  # just need to silence the warning
+  x <- tempfile(fileext = ".zip")
+  file.create(x)
+  expect_warning(check_xlsx_path(x))
+  expect_warning(check_xlsx_path(x, warn = FALSE), NA)
   file.remove(x)
 })
