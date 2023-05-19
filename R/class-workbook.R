2c2a908b29e4f09880b6525198c72b6b96389081
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2023-05-19 13:12:20 +0200

    add wb_add_ignore_error()

--- R/class-workbook.R
+++ R/class-workbook.R
@@ -6219,6 +6219,47 @@ wbWorkbook <- R6::R6Class(
       sheet <- private$get_sheet_index(sheet)
       self$worksheets[[sheet]]$add_sparklines(sparklines)
       invisible(self)
+    },
+
+    #' @description Ignore error on worksheet
+    #' @param sheet sheet
+    #' @param dims dims
+    #' @param calculatedColumn calculatedColumn
+    #' @param emptyCellReference emptyCellReference
+    #' @param evalError evalError
+    #' @param formula formula
+    #' @param formulaRange formulaRange
+    #' @param listDataValidation listDataValidation
+    #' @param numberStoredAsText numberStoredAsText
+    #' @param twoDigitTextYear twoDigitTextYear
+    #' @param unlockedFormula unlockedFormula
+    add_ignore_error = function(
+      sheet              = current_sheet(),
+      dims               = "A1",
+      calculatedColumn   = FALSE,
+      emptyCellReference = FALSE,
+      evalError          = FALSE,
+      formula            = FALSE,
+      formulaRange       = FALSE,
+      listDataValidation = FALSE,
+      numberStoredAsText = FALSE,
+      twoDigitTextYear   = FALSE,
+      unlockedFormula    = FALSE
+    ) {
+      sheet <- private$get_sheet_index(sheet)
+      self$worksheets[[sheet]]$ignore_error(
+        dims               = dims,
+        calculatedColumn   = calculatedColumn,
+        emptyCellReference = emptyCellReference,
+        evalError          = evalError,
+        formula            = formula,
+        formulaRange       = formulaRange,
+        listDataValidation = listDataValidation,
+        numberStoredAsText = numberStoredAsText,
+        twoDigitTextYear   = twoDigitTextYear,
+        unlockedFormula    = unlockedFormula
+      )
+      invisible(self)
     }
 
   ),
