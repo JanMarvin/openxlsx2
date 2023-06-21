84563bf1e66dba6cdeaa41e2cc8d9d0eecaea6ba
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2023-06-21 23:49:28 +0200

    add dims to wb_add_conditional_formatting(). closes #654 (#655)

--- tests/testthat/test-conditional_formatting.R
+++ tests/testthat/test-conditional_formatting.R
@@ -789,4 +789,13 @@ test_that("conditional formatting with gradientFill works", {
   got <- wb$styles_mgr$styles$dxfs
   expect_equal(exp, got)
 
+  # check that the wrapper handles dims as well
+  expect_silent(
+    wb_workbook() %>%
+    wb_add_worksheet() %>%
+    wb_add_conditional_formatting(dims = "A1:B5",
+                                  type = "between",
+                                  rule = c(2, 4))
+  )
+
 })
