a3939c1c02407e87b9728b2114be05cc62273335
olivroy <olivierroy71@hotmail.com>
2023-07-11 13:18:42 -0400

    Move `expect_equal_workbooks()` to `tests/testthat/helper.R`

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,3 +1,42 @@
+#' expect_equal_workbooks() ------------------------------------------
+#' Quick internal expectation function.  Easier to ignore fields to be set
+expect_equal_workbooks <- function(object, expected, ..., ignore_fields = NULL) {
+
+  # object <- as.list(object)
+  # expected <- as.list(expected)
+
+  assert_workbook(object)
+  assert_workbook(expected)
+
+  fields <- c(names(wbWorkbook$public_fields), names(wbWorkbook$private_fields))
+  bad <- setdiff(ignore_fields, fields)
+
+  if (length(bad)) {
+    stop("Invalid fields: ", toString(bad))
+  }
+
+  for (i in ignore_fields) {
+    object[[i]] <- NULL
+    expected[[i]] <- NULL
+  }
+
+  bad <- waldo::compare(
+    x     = object,
+    y     = expected,
+    x_arg = "object",
+    y_arg = "expected",
+    ...
+  )
+
+  if (length(bad)) {
+    testthat::fail(bad)
+    return(invisible())
+  }
+
+  testthat::succeed()
+  return(invisible())
+}
+
 # expect_wrapper helper --------------------
 #' Expect wrapper
 #'
