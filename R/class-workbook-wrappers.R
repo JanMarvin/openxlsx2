4ee383b426db3a7a968e3513cd493a26a1047d55
Jan Marvin Garbuszus <jan.garbuszus@rub.de>
2024-06-18 09:37:16 +0200

    [doc] add x14 icon sets

--- R/class-workbook-wrappers.R
+++ R/class-workbook-wrappers.R
@@ -3873,9 +3873,9 @@ wb_add_form_control <- function(
 #'     `[params$reverse]`\cr If `TRUE`, the order is reversed. Default `FALSE`\cr\cr
 #'     `[params$percent]`\cr If `TRUE`, uses percentage\cr\cr
 #'     `[params$iconSet]`\cr Uses one of the implemented icon sets. Values must match the length of the icons
-#'      in the set 3Arrows, 3ArrowsGray, 3Flags, 3Signs, 3Symbols, 3Symbols2, 3TrafficLights1, 3TrafficLights2,
-#'      4Arrows, 4ArrowsGray, 4Rating, 4RedToBlack, 4TrafficLights, 5Arrows, 5ArrowsGray, 5Quarters, 5Rating. The
-#'      default is 3TrafficLights1.
+#'      in the set 3Arrows, 3ArrowsGray, 3Flags, 3Signs, 3Stars, 3Symbols, 3Symbols2, 3TrafficLights1, 3TrafficLights2,
+#'      3Triangles, 4Arrows, 4ArrowsGray, 4Rating, 4RedToBlack, 4TrafficLights, 5Arrows, 5ArrowsGray, 5Boxes, 5Quarters, 5Rating.
+#'      The default is 3TrafficLights1.
 #'  }
 #' }
 #'
