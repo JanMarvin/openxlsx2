54f891e6617c43a73a1f36ce0457c2053744eab1
Jan Marvin Garbuszus <jan.garbuszus@ruhr-uni-bochum.de>
2025-04-14 22:32:56 +0200

    [styles] set default value for create_fill() (#1321)
    * [styles] set default value for create_fill()
    
    fixes #1320
    
    * [styles] set default values for create_numfmt()

--- R/wb_styles.R
+++ R/wb_styles.R
@@ -228,7 +228,7 @@ create_border <- function(
 #' )
 #'
 #' @export
-create_numfmt <- function(numFmtId, formatCode) {
+create_numfmt <- function(numFmtId = 164, formatCode = "#,##0.00") {
 
   # in dates a slash does not indicate a fraction, but can be used to
   # separate year, month, day
@@ -464,6 +464,7 @@ create_fill <- function(
   # if gradient fill is specified we can not have patternFill too. otherwise
   # we end up with a solid black fill
   if (gradient_fill == "") {
+    if (pattern_type == "") pattern_type <- "none"
     valid_pattern <- c("none", "solid", "mediumGray", "darkGray", "lightGray", "darkHorizontal", "darkVertical", "darkDown", "darkUp", "darkGrid", "darkTrellis", "lightHorizontal", "lightVertical", "lightDown", "lightUp", "lightGrid", "lightTrellis", "gray125", "gray0625")
     match.arg_wrapper(pattern_type, valid_pattern, fn_name = "create_fill")
 
