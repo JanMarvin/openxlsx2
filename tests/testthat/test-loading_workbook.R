JanMarvin/openxlsx2

commit e4f0d392282de0946c97bc78f8f49b1edeb8553d

[load] avoid initialization of wb$vml_rels until needed (#1557)
* [load] avoid initialization of wb$vml_rels until needed

closes #1556

* [comments] speedup wb_get_comments()

fixes #1555

R/wb_load.R
@@ -1333,8 +1333,6 @@ wb_load <- function(
       vml_len <- max(as.integer(gsub("\\D+", "", basename(vmlDrawingXML))))
 
       wb$vml      <- rep(list(""), vml_len) # vector("list", vml_len)
-      wb$vml_rels <- rep(list(""), vml_len) # vector("list", vml_len)
-
 
       for (vml in vmlDrawingXML) {
 
@@ -1348,6 +1346,7 @@ wb_load <- function(
       }
 
       for (vml_rel in vmlDrawingRelsXML) {
+        wb$vml_rels <- rep(list(""), vml_len) # vector("list", vml_len)
 
         vml_file <- as.integer(gsub("\\D+", "", basename(vml_rel)))
 

R/class-workbook.R
@@ -5646,24 +5646,33 @@ wbWorkbook <- R6::R6Class(
 
       cmts <- list()
       if (length(cmmt) && length(self$comments) >= cmmt) {
-        cmts <- do.call(rbind, lapply(self$comments[[cmmt]], function(x) {
+
+        comments <- self$comments[[cmmt]]
+
+        if (!is.null(dims)) {
+          all_refs <- vapply(comments, `[[`, "ref", FUN.VALUE = NA_character_)
+          idx <- which(all_refs %in% dims)
+        } else {
+          # If dims is NULL, take everything
+          idx <- seq_along(comments)
+        }
+
+        if (length(idx) == 0) return(NULL)
+
+        cmts <- do.call(rbind, lapply(idx, function(i) {
+          x <- comments[[i]]
           data.frame(
             ref = x$ref,
             author = x$author,
             comment = paste0(x$comment, collapse = " "),
+            # Store the original index as the row name so add_thread() can find it
+            row.names = i,
             stringsAsFactors = FALSE
           )
         }))
 
-        if (!is.null(dims)) cmts <- cmts[cmts$ref %in% dims, ]
-        # print(cmts)
-        cmts <- cmts[c("ref", "author", "comment")]
-        if (NROW(cmts)) {
-          cmts$comment <- as_fmt_txt(cmts$comment)
-          cmts$cmmt_id <- cmmt
-        } else {
-          return(NULL)
-        }
+        cmts$comment <- as_fmt_txt(cmts$comment)
+        cmts$cmmt_id <- cmmt
       }
 
       cmts
@@ -5727,7 +5736,7 @@ wbWorkbook <- R6::R6Class(
       }
 
       sheet <- private$get_sheet_index(sheet)
-      wb_cmt <- wb_get_comment(self, sheet, dims)
+      wb_cmt <- self$get_comment(sheet, dims)
 
       if (length(cmt <- wb_cmt$comment)) {
         # TODO not sure yet what to do
@@ -5735,7 +5744,7 @@ wbWorkbook <- R6::R6Class(
         cmt <- wb_comment(text = comment, author = "")
         self$add_comment(sheet = sheet, dims = dims, comment = cmt)
       }
-      wb_cmt <- wb_get_comment(self, sheet, dims)
+      wb_cmt <- self$get_comment(sheet, dims)
 
       if (!length(self$worksheets[[sheet]]$relships$threadedComment)) {
 
@@ -5816,7 +5825,7 @@ wbWorkbook <- R6::R6Class(
 
         if (reply) cmt_id <- parentId
 
-        wb_cmt <- wb_get_comment(self, sheet, dims)
+        wb_cmt <- self$get_comment(sheet, dims)
         sId <- wb_cmt$cmmt_id
         cId <- as.integer(rownames(wb_cmt))
 

tests/testthat/test-read_sources.R
@@ -82,7 +82,13 @@ test_that("read vml from sheet two works as expected", {
   expect_silent(wb <- wb_load(testfile_path("vml_numbering.xlsx")))
 
   expect_equal(length(wb$vml), 1L)
-  expect_equal(length(wb$vml_rels), 1L)
+  expect_equal(wb$vml_rels, NULL)
+
+  # extend test for https://github.com/JanMarvin/openxlsx2/issues/1556
+  wb$remove_comment(sheet = 2)
+  tmp <- temp_xlsx()
+  on.exit(unlink(tmp, recursive = TRUE), add = TRUE)
+  expect_silent(wb$save(tmp))
 
 })
 

commit a76879ef417c748f65a8b046e4b1bea2ded00fd6

[load] fix wb$vml_rels re-init inside loop dropping all but last .vml.rels
wb$vml_rels was re-initialised on every iteration of the
vmlDrawingRelsXML loop, so only the last worksheet's vmlDrawing.vml.rels
survived a wb_load() + wb_save() round-trip (the others became dangling
o:relid references, triggering Excel's repair prompt).

Move the (length-guarded) initialisation out of the loop, mirroring the
wb$vml init just above. Regression introduced in #1557 (v1.24).

R/wb_load.R
@@ -1386,8 +1386,10 @@ wb_load <- function(
         wb$vml[vml_file] <- read_xml(vml, pointer = FALSE)
       }
 
-      for (vml_rel in vmlDrawingRelsXML) {
+      if (length(vmlDrawingRelsXML))
         wb$vml_rels <- rep(list(""), vml_len) # vector("list", vml_len)
+
+      for (vml_rel in vmlDrawingRelsXML) {
 
         vml_file <- cdigit(basename(vml_rel), as_integer = TRUE)
 

inst/AUTHORS
@@ -32,6 +32,7 @@ Miriam Rainers
 Moritz Lell
 Noam Ross
 olivroy
+Paul Schmidt
 Philipp Schauberger
 Reinhold Kainhofer
 Renaud

tests/testthat/test-loading_workbook.R
@@ -742,3 +742,43 @@ test_that("has_drawings is defunct", {
     "argument \"has_drawing\" was removed"
   )
 })
+
+test_that("loading keeps every vmlDrawing.vml.rels (#1557 regression)", {
+
+  # A comment with a background image produces a vmlDrawing*.vml.rels per
+  # sheet. wb$vml_rels used to be re-initialised inside the load loop, so all
+  # but the last sheet's .vml.rels were dropped on a load + save round-trip,
+  # leaving dangling o:relid references (Excel repair prompt).
+  img <- system.file("extdata", "einstein.jpg", package = "openxlsx2")
+
+  wb <- wb_workbook()
+  for (i in 1:3) {
+    wb$add_worksheet(paste0("S", i))
+    wb$add_comment(
+      sheet   = i,
+      dims    = "B2",
+      comment = wb_comment(text = paste("comment", i), author = "x"),
+      file    = img
+    )
+  }
+
+  count_vmlrels <- function(f) {
+    sum(grepl("xl/drawings/_rels/vmlDrawing.*\\.vml\\.rels$",
+              unzip(f, list = TRUE)$Name))
+  }
+
+  tmp1 <- temp_xlsx()
+  on.exit(unlink(tmp1), add = TRUE)
+  wb$save(tmp1)
+  expect_equal(count_vmlrels(tmp1), 3)
+
+  wb2 <- wb_load(tmp1)
+  # all three entries survive the load (previously only the last was kept)
+  expect_equal(sum(vapply(wb2$vml_rels, function(x) !all(x == ""), NA)), 3)
+
+  tmp2 <- temp_xlsx()
+  on.exit(unlink(tmp2), add = TRUE)
+  wb2$save(tmp2)
+  expect_equal(count_vmlrels(tmp2), 3)
+
+})